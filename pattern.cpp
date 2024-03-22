e3e2085af9578e5dcf0823bbf8620d3b239ed674
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-03-22 23:39:49 +0530

    pattern 8 reverse triangle

--- pattern.cpp
+++ pattern.cpp
@@ -70,17 +70,36 @@ void print6(int n)
 }
 void print7(int n)
 {
-     for (int i = 0; i < n; i++) {
-        // space
-        for (int j = 0; j < n - i - 1; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
-        // star
-        for (int j = 0; j < 2 * i + 1; j++) {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
             cout << "*";
         }
-        // space
-        for (int j = 0; j < n - i - 1; j++) {
+        // for(int j = 0; j < n-i-1; j++){
+        //     cout << " ";
+        // }
+        cout << endl;
+    }
+}
+void print8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
             cout << " ";
         }
         cout << endl;
@@ -91,5 +110,5 @@ int main()
 {
     int n;
     cin >> n;
-    print7(n);
+    print8(n);
 }
